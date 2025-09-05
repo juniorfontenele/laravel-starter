1ec7b7a674c2f1a8d93c332a2f21c559472c4942
Junior Fontenele <github@juniorfontenele.com.br>
2025-09-05 20:12:27 -0300

    feat: add HasUuids trait to User model for UUID support

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -15,6 +16,7 @@ class User extends Authenticatable
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
     use Notifiable;
+    use HasUuids;
 
     /**
      * The attributes that are mass assignable.
